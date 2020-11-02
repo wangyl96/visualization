85631ebf8de6306cd9556007778737036e51c1ee
wangyl <wangyiliang@pt.picc.com.cn>
2020-11-02 10:45:50 +0800

    项目搭建,新增业务服务

---
+++ visualization-business-parent/visualization-business-api/src/main/java/com/sinosoft/visualization/business/api/entity/Hello.java
@@ -0,0 +1,12 @@
+package com.sinosoft.visualization.business.api.entity;
+
+/**
+ * Hello
+ *
+ * @author wangyl
+ * Description:
+ * Created in: 2020/11/2
+ * Modified by:
+ */
+public class Hello {
+}
---
+++ visualization-business-parent/visualization-business/src/main/java/com/sinosoft/visualization/business/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.sinosoft.visualization.business.controller;
+
+/**
+ * HelloController
+ *
+ * @author wangyl
+ * Description:
+ * Created in: 2020/11/2
+ * Modified by:
+ */
+public class HelloController {
+}
